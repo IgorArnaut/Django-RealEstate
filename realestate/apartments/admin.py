6db03fa0a4e61a4b3891c4c80d9a6f8bfc184a9a
Igor <igor.arnautovic123@outlook.com>
2024-12-12 19:08:17 +0100

    Added Landlord and Agency models

--- realestate/apartments/admin.py
+++ realestate/apartments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Agency, Apartment, Condition, Content, Landlord
+
 # Register your models here.
+admin.site.register(Apartment)
+admin.site.register(Condition)
+admin.site.register(Content)
+admin.site.register(Agency)
+admin.site.register(Landlord)
--- realestate/apartments/models.py
+++ realestate/apartments/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
 STATE_CHOICES = {
@@ -23,22 +24,22 @@ HEATING_CHOICES = {
 
 
 class Condition(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=16)
 
     def __str__(self):
         return f"{self.name}"
 
 
 class Content(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=16)
 
     def __str__(self):
         return f"{self.name}"
 
 
 class Apartment(models.Model):
-    numOfRooms = models.PositiveSmallIntegerField()
-    location = models.CharField(max_length=20)
+    num_of_rooms = models.PositiveSmallIntegerField()
+    location = models.CharField(max_length=32)
     price = models.PositiveIntegerField()
     area = models.PositiveIntegerField()
     story = models.PositiveSmallIntegerField()
@@ -51,3 +52,30 @@ class Apartment(models.Model):
 
     def __str__(self):
         return f"{self.location} {self.price} {self.story}"
+
+
+class Profile(AbstractBaseUser):
+    phone = models.CharField(max_length=16)
+
+
+class Landlord(Profile):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Agency(Profile):
+    name = models.CharField(max_length=32)
+    website = models.CharField(max_length=48)
+    email = models.EmailField(max_length=64)
+    registration_date = models.DateField()
+
+
+    class Meta:
+        verbose_name_plural = "Agencies"
+
+
+    def __str__(self):
+        return f"{self.name}"
