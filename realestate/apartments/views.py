adc36463ae7939973734e71c74a9c50887b17bfb
Igor <igor.arnautovic123@outlook.com>
2025-01-02 13:43:41 +0100

    Added some views

--- realestate/apartments/views.py
+++ realestate/apartments/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 
+from .models import Ad
+
 # Create your views here.
 def index(request):
-    return render(request, 'apartments/index.html')
+    ads = Ad.objects.all()
+    context = {
+        'ads': ads
+    }
+    return render(request, 'apartments/index.html', context)
 
 
 def search(request):
     return render(request, 'apartments/search.html')
+
+
+def detail(request, id):
+    ad = Ad.objects.get(pk=id)
+    return render(request, 'apartments/detail.html', { 'ad': ad })
